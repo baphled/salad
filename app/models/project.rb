5cf7c5a3af94538c6b6d5149472b18c389d09ae9
Yomi Colledge <baphled@boodah.net>
2009-07-19 07:37:35 +0100

    added presence of am to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :description
+  validates_presence_of :aim
 end
--- features/step_definitions/projects_steps.rb
+++ features/step_definitions/projects_steps.rb
@@ -22,6 +22,11 @@ When /^don't fill in the description$/ do
   fill_in 'project_aim', :with => 'the aim of our project is...'
 end
 
+When /^don't fill in the aim$/ do
+  fill_in 'project_title', :with => 'A project'
+  fill_in 'project_description', :with => 'This is a description'
+end
+
 Then /^submit the form$/ do
   click_button 'Submit'
 end
