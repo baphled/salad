f79be66f52afeef0901a8a7e73302d193c29576d
Yomi Colledge <baphled@boodah.net>
2009-07-31 18:36:19 +0100

    More work on the layout and a couple of typo fixes.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function display_project_info($id) {
+	$("#display").hide();
+	$("li.show_info_" + $id).click(function() {
+		$(".displayed").animate({opacity: 'toggle',height: 'slide'},'slow');
+		$(".info_" + $id).animate({opacity: 'toggle',height: 'slide'},'slow');
+		return false;
+	});
+});
