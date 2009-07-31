a28c81eec5f1e42d457cac69e75684aeb1aebefc
Yomi Colledge <baphled@boodah.net>
2009-07-31 17:24:21 +0100

    Added validations.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,8 @@
 class Story < ActiveRecord::Base
+  validates_presence_of     :title
+  validates_presence_of     :scenario
+  validates_uniqueness_of   :title
+  
   has_many :feature_stories
   has_many :features, :through => :feature_stories
 end
