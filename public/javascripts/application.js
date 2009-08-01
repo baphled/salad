0926fb6f6f005d58bab9972d5602ccae9a5905e7
Yomi Colledge <baphled@boodah.net>
2009-08-01 02:20:13 +0100

    Added functionality to customise our error handling, also made some small changes to the CSS.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function display_project_info($id) {
+function() {
 	$("#display").hide();
 	$("li.show_info_" + $id).click(function() {
 		$(".displayed").animate({opacity: 'toggle',height: 'slide'},'slow');
