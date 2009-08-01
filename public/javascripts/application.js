68fa9794c10e9be9ef428871cda0ce1cfa27a9f6
Yomi Colledge <baphled@boodah.net>
2009-08-01 16:37:34 +0100

    Added basic functionality to display a features stories on the right hand side.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-function() {
-	$("#display").hide();
-	$("li.show_info_" + $id).click(function() {
-		$(".displayed").animate({opacity: 'toggle',height: 'slide'},'slow');
-		$(".info_" + $id).animate({opacity: 'toggle',height: 'slide'},'slow');
-		return false;
-	});
-});
+function displayFeatureInfo(e, reg) {
+	$(reg).toggle().visible();
+}
