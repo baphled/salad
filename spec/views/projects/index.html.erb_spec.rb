038a84c6e48bd9a48a87cce2e266a5d185fbe466
Yomi Colledge <baphled@boodah.net>
2009-08-03 05:01:48 +0100

    More code cleanup.

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -9,7 +9,7 @@ describe "/projects/index.html.erb" do
     end
     
     it "should display a message telling the user no projects are available" do
-      response.should have_selector :span, :content => "No projects available"
+      response.should have_selector :span, :content => "No projects present."
     end
   end
   
