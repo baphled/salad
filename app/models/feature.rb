69586411624d3a440943cfca667e630a2a7df0ef
Yomi Colledge <baphled@boodah.net>
2009-08-11 05:28:04 +0100

    Refactored model so that our output is displayed in the same fashion as cucumber.
    Users can now create features via the webapp and export them to cucumber.

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -15,17 +15,31 @@ class Feature < ActiveRecord::Base
   def export
     @exported = feature_title
     self.stories.each do |story|
-      if not story.steps.blank?
-        @exported += "  Scenario: #{story.title}\n"
-        story.steps.each do |step|
-          @exported += "    #{step.title}\n"
-        end
-      end
+      feature_scenarios story
     end
     @exported
   end
   
   private
+    def feature_scenarios story
+      if not story.steps.blank?
+        @exported += "  Scenario: #{story.title}\n"
+        story_titles story
+      end
+    end
+    
+    def story_titles story
+     story.steps.each do |step|
+       if !@last_step.nil? && @last_step.title.split(" ").first == step.title.split(" ").first
+         title = step.title.sub(step.title.split(" ").first, "And")
+         @exported += "    #{title}\n"
+       else
+         @exported += "    #{step.title}\n"
+       end
+       @last_step = step
+     end
+    end
+    
     def feature_title
       @head =     "Feature: #{title}\n  In order #{in_order}\n"
       @head +=    "  As a #{as_a}\n  I want #{i_want}\n\n"
