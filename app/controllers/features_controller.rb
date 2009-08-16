838c9e21b56399d70fd07efc22653d995e1d5007
Yomi Colledge <baphled@boodah.net>
2009-08-16 16:06:32 +0100

    Cleanup of the new action in features controller.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -12,8 +12,8 @@ class FeaturesController < ApplicationController
   
   def new
     if !params[:project_id].nil? && !Project.find(params[:project_id]).nil?
-      @feature = Feature.new
-      @feature.projects << Project.find(params[:project_id])
+      @project = Project.find(params[:project_id])
+      @feature = @project.feature.new
     else
       @feature = Feature.new
     end
