d0ec5c499febce65a8b2493bffcfcef636974f17
Yomi Colledge <baphled@boodah.net>
2009-08-18 03:47:38 +0100

    Added uniqueness validations for a stories scenario.

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,6 +2,7 @@ class Story < ActiveRecord::Base
   acts_as_taggable
   
   validates_presence_of     :scenario
+  validates_uniqueness_of   :scenario
   
   has_many :feature_stories
   has_many :features, :through => :feature_stories
