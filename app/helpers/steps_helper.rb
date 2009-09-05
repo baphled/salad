568f7d4785b45ae689c3ab3b8661966ef74c7b60
Yomi Colledge <baphled@boodah.net>
2009-09-05 23:45:12 +0100

    Refactored to clean up duplication of the code base.

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,14 +1,10 @@
 module StepsHelper
+  include ApplicationHelper
   def viewable_link link_text, view_id
     render :partial => '/common/viewable_link', 
     			 :locals => {:view_id => view_id, :link_text => link_text}
   end
   
-  def order_btn object, order
-    if !object.blank?
-      render :partial => '/common/order', :locals => {:order => order}
-  	end
-  end
   
   def format_step step
     @_title = Feature::format_step step,@last_step
