2bd7421d26db485b47502bceb8d89bc620ce8d61
Yomi Colledge <baphled@boodah.net>
2009-09-05 22:48:18 +0100

    Modified pagination js as we can directly load it as soon as the DOM is ready.

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,11 +1,7 @@
-var Pagination = function() {
+$(function() {
 	$(".pagination a").live("click", function(){
 		$(".pagination").html("Page loading...");
 		$.get(this.href, null, null, "script");
 		return false;
 	});
-};
-
-$(function() {
-	Pagination();
 });
