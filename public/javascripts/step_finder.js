dc3e5fc234ecb87aa49f06767e20f2984efdea57
Yomi Colledge <baphled@boodah.net>
2009-10-25 13:42:02 +0000

    Removed prototype based JS files and cleaned up step finder.

--- public/javascripts/step_finder.js
+++ public/javascripts/step_finder.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-	var $search = $('#search').addClass('overlabel');
-	var $searchInput = $search.find('input');
-	var $searchLabel = $search.find('label');
-	
-	var $autoComplete = $('<ul class="autocomplete"></ul>')
-			.hide()
-			.insertAfter('#search_text');
-			
+
 	$('#search_text').keyup(function() {
 		$.ajax({
 			'url': '/steps',
