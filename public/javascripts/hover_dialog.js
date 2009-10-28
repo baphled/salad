a743fb5b7970e02dd10ea6fd5c0d4b2294dc244a
Yomi Colledge <baphled@boodah.net>
2009-10-28 10:57:35 +0000

    More cleanup of the code, to make it cleaner & more readable.

--- public/javascripts/hover_dialog.js
+++ public/javascripts/hover_dialog.js
@@ -1,13 +1,18 @@
+// Functionality to create hover dialog boxes for our tag input
 $(document).ready(function() {
   
+  // Create acessors for our hover dialog
   var $formAction = $('form').attr('action');
   var $formID = $('form').attr('id');
   
+  // We need singular & plural resource names to take advantage of hover dialog
   var $resourceSingular = $formID.substring(4,$formID.length);
   var $resourcePlural = $formAction.substring(1,$formAction.length);
   
+  // Selector for our tag input
   var $tagInput = $('input#'+ $resourceSingular + '_tag_list');
   
+  // The hover dialog we'll use to populate our tags
   var $hoverDialog = $("<div></div>")
       .addClass('hover')
       .addClass('ui-widget')
@@ -15,7 +20,8 @@ $(document).ready(function() {
       .addClass('ui-corner-all')
       .insertAfter($tagInput)
       .hide();
-      
+  
+  // Container that our tags are wrapped in
   var $hoverSelect = $('<li></li>').addClass('hover_select');
 
   $tagInput.focus(function() {
@@ -26,22 +32,23 @@ $(document).ready(function() {
     	'type': 'GET',
     	'success': function(data) {
     		if (data.length) {
-    		  $hoverDialog.empty();
-    			$.each(data, function(index, item) {
-    			  $hoverDialog.append($('<a href="javascript:///>"')
-    			    .append(item['tag']['name'])
-    			    .addClass('hover_select')
-    			    .attr('id','item_' + item['tag']['id'])
-    			    .click(function() {
-    			      $tagInput.attr('value',$tagInput.attr('value') + $(this).html() + ', ');
-    			      $(this).fadeOut();
-                return false;
-              }))
-    			    .append(' ');
-    			});
+    		  if($hoverDialog.empty()) {
+      			$.each(data, function(index, item) {
+      			  $hoverDialog.append($('<a href="javascript:///>"')
+      			    .append(item['tag']['name'])
+      			    .addClass('hover_select')
+      			    .attr('id','item_' + item['tag']['id'])
+      			    .click(function() {
+      			      $tagInput.attr('value',$tagInput.attr('value') + $(this).html() + ', ');
+      			      $(this).fadeOut();
+                  return false;
+                }))
+      			    .append(' ');
+      			});
+    		  }
     		}
     	}
-    });
+    });    
     $('.hover').fadeIn();
   });
 });
