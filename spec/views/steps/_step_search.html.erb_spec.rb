200d85475cefa85e7760784cf0edbdb6dfa54f93
Yomi <yomi@MyBuilders-iMac.local>
2009-12-16 14:57:52 +0000

    Added partial spec, which should of been added ages ago.

---
+++ spec/views/steps/_step_search.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/steps/_step_search.html.erb" do
+  before(:each) do
+    assigns[:steps] = Step.pagination_search "Given",1
+    assigns[:search] = Step.search "Given"
+    render :locals => {:steps => @steps, :order =>false}
+  end
+  
+  describe "GET, search" do
+    it "should have a form" do
+      response.should have_selector :form
+    end
+    
+    it "should have a text field" do
+      response.should have_selector :form do |form|
+        form.should have_selector :input, attribute = {:type=>"text"}
+      end
+    end
+    
+    it "should allow us to search for steps" do
+      response.should have_selector :form do |form|
+        form.should have_selector :input, attribute = {:type=>"submit"}
+      end
+    end
+    
+    it "should display the number of results found" do
+      response.should contain "Results (#{assigns[:steps].size.to_s})"
+    end
+  end
+end
