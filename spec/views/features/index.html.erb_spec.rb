4f8b135719f207a0dac5ec63c714fc5d0c669d9c
baphled <baphled@meedihoe.bo0dah>
2010-01-10 21:50:28 +0000

    More spec cleanup

--- spec/views/features/index.html.erb_spec.rb
+++ spec/views/features/index.html.erb_spec.rb
@@ -2,8 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/features/index.html.erb" do
   before(:each) do
-    assigns[:tags] = Feature.tag_counts
-    assigns[:features] = Feature.all
+    assigns[:tags] = []
+    assigns[:features] = [stub_model(Feature,:title => 'nu', :created_at => Time.now).as_null_object,
+                          stub_model(Feature,:title => 'suttin else', :created_at => Time.now).as_null_object]
     render
   end
   
@@ -25,7 +26,7 @@ describe "/features/index.html.erb" do
     end
     
     it "should have a list of features " do
-      Feature.all.each do |feature|
+      assigns[:features].each do |feature|
         response.should have_selector :div do |content|
           content.should contain feature.title
         end
@@ -36,7 +37,7 @@ describe "/features/index.html.erb" do
   
   context "feature with stories associated to it" do
     it "should have a view stories link" do
-      response.should have_selector :a, attribute = {:href=>"/features/1/stories"}
+      response.should have_selector :a, attribute = {:href=> feature_path(assigns[:features].first)}
     end
   end
 end
