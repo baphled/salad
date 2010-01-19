185de491a80304b7f399236d62e15f0807270189
Yomi Colledge <baphled@boodah.net>
2010-01-19 23:51:31 +0000

    General cleanup

--- features/step_definitions/selenium_steps.rb
+++ features/step_definitions/selenium_steps.rb
@@ -1,7 +1,7 @@
 When /^the first project is hovered over$/ do
-  selenium.mouse_over("project_2")
+  selenium.mouse_over("project_1")
 end
 
 Then /^the project's information will be display in the sidebar$/ do
-  selenium.wait_for_visible("project_2_feature")
+  selenium.wait_for_visible("project_1_feature")
 end
