decc62335e9586a80397e24b3b1bf9c36c77a05b
Yomi Colledge <baphled@boodah.net>
2010-01-20 04:48:23 +0000

    Commented out the global start ups, as we only really need to initialise selenium

--- features/support/enhanced.rb
+++ features/support/enhanced.rb
@@ -49,12 +49,12 @@ World(Webrat::Selenium::Matchers)
 
 # "before all"
 browser = Selenium::SeleniumDriver.new("localhost", 4444, "*chrome", "http://localhost", 15000)
-
-Before do
-  @browser = browser
-  @browser.start
-end
-
-After do
-  @browser.stop
-end
+#
+#Before do
+#  @browser = browser
+#  @browser.start
+#end
+#
+#After do
+#  @browser.stop
+#end
