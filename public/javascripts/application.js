0311ae4dce08dc118b1f27ab53c89e3e57d92f17
Yomi Colledge <baphled@boodah.net>
2010-01-21 17:54:01 +0000

    Added functionality to globally add an auth token. so now we can create more unobtrusive JQuery.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,11 +18,24 @@ function displayInfo(event,selector) {
   $('#' +selector).toggle();
 }
 
-/**
-	Used to toggle our order functionality
-**/
+// This sets up the proper header for rails to understand the request type,
+// and therefore properly respond to js requests (via respond_to block, for example)
+$.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+});
 
 $(document).ready(function() {
+  // UJS authenticity token fix: add the authenticy_token parameter
+  // expected by any Rails POST request.
+  $(document).ajaxSend(function(event, request, settings) {
+    // do nothing if this is a GET request. Rails doesn't need
+    // the authenticity token, and IE converts the request method
+    // to POST, just because - with love from redmond.
+    if (settings.type == 'GET') return;
+    if (typeof(AUTH_TOKEN) == "undefined") return;
+    settings.data = settings.data || "";
+    settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+  });
   
   $('#myTabs').tabs();
 	
