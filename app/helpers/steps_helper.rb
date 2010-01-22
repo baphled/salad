29b68a769c6c2a68c1f26ab27322dfe789bff649
Yomi Colledge <baphled@boodah.net>
2010-01-22 23:43:54 +0000

    Removed no longer needed helpers & partial.

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,9 +1,3 @@
 module StepsHelper
   include ApplicationHelper
-  
-  def viewable_link link_text, view_id
-    render :partial => '/common/viewable_link', 
-    			 :locals => {:view_id => view_id, :link_text => link_text}
-  end
-  
 end
