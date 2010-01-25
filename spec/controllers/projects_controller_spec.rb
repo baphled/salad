09fbaa01068438bb023227e42631863d87d37e7e
Yomi Colledge <baphled@boodah.net>
2010-01-25 17:11:54 +0000

    Added pending specs for the projects controller.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -4,6 +4,16 @@ describe ProjectsController do
   before(:each) do
     @project = mock_model(Project).as_new_record.as_null_object
   end
+
+  describe "GET, index" do
+    it "should get a list of all projects"
+  end
+
+  describe "GET, new" do
+    it "renders the new form template"
+    it "gets a list of features"
+  end
+
   describe "POST, create" do
     before(:each) do
       Project.stub!(:new).and_return @project
@@ -18,6 +28,11 @@ describe ProjectsController do
       it "should display a flash message" do
         post :create
         flash.should contain "Project: #{@project.title} was created"
+      end
+
+      it "should redirect to the projects page" do
+        post :create
+        response.should redirect_to projects_path
       end
     end
 
@@ -38,4 +53,48 @@ describe ProjectsController do
     end
   end
 
+  describe "GET, edit" do
+    it "should get the related project"
+    it "should render the edit form template"
+  end
+
+  describe "PUT, update" do
+    context "successfully update a project" do
+      it "should update the project"
+      it "should display a flash message"
+      it "should redirect to the project"
+    end
+
+    context "unsuccessfully update a project" do
+      it "should not update the project"
+      it "should display a flash[:error] message"
+      it "should render the edit form template"
+    end
+  end
+
+  describe "GET, show" do
+    it "should get the project"
+  end
+
+  describe "DELETE, destroy" do
+    it "should find the project"
+    it "should destroy the project"
+    it "should redirect to the projects path"
+  end
+
+  describe "GET, features" do
+    it "should get the project"
+    it "should get all features related to the project"
+  end
+
+  dscribe "GET, import" do
+    it "should find the project"
+    it "should create a feature associated to the project"
+    it "should retreive a list of features to import"
+  end
+
+  describe "GET, tag" do
+    it "should find all projects with the given tag"
+    it "shold render the index page"
+  end
 end
