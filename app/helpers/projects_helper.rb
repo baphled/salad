e1e55900a8b98cff48bd176e6ca1be4db80f6c0c
Yomi Colledge <baphled@boodah.net>
2010-01-25 16:20:58 +0000

    Removed uneeded helper methods.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,20 +1,4 @@
 module ProjectsHelper
-  def format_step step
-    if step.include?("And")
-      check_step_prefix
-  	end
-  end
-  
-  def check_step_prefix
-    w%("Given, When, Then").each do |prefix|
-      if @last.include?(prefix)
-  			step.sub(/And /,"#{prefix} ")
-    	else
-    		step
-    	end
-  	end
-  end
-  
   def steps_imported? list
     list.each do |file|
       if Feature.find_by_title(file[:feature].title).nil?
