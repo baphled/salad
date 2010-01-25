76323ad93f171c8960561a4f77c51c74499a0c7b
Yomi Colledge <baphled@boodah.net>
2010-01-25 16:36:34 +0000

    General cleanup.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,24 +3,29 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe ProjectsController do
   
   describe "POST, create" do
-      context "project successfully" do
-      
+    before(:each) do
+      @project = mock_model(Project).as_new_record.as_null_object
+    end
+    context "project successfully created" do
       before(:each) do
-        @project = mock_model(Project,:title=>"A project")
-        Project.stub!(:new).and_return @project        
+        Project.stub!(:new).and_return @project
       end
       
       it "should save the project" do
-        @project.should_receive :save
-        post :create
+        @project.should_receive(:save).and_return true
+        post :create, :project => @project
       end
     
       it "should display a flash message" do
-        @project.stub!(:save).and_return true
-        post :create
-        flash.should contain "A project was created"
+        post :create, :project => @project
+        flash.should contain "Project: #{@project.title} was created"
       end
     end
+  end
+
+  context "project unsuccessfully created" do
+    it "should not save the project"
+    it "should render the new form"
   end
   
 end
