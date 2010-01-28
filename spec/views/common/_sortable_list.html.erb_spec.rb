1be1ac128517eb7bf2bd1e54c16fd7cd86357e25
Yomi Colledge <baphled@boodah.net>
2010-01-28 17:09:48 +0000

    Added specs to make sure that there is an error if the required locals are not assigned.
    Should ideally log these error.

--- spec/views/common/_sortable_list.html.erb_spec.rb
+++ spec/views/common/_sortable_list.html.erb_spec.rb
@@ -4,11 +4,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "common/_sortable_list.html.erb" do
-  before(:each) do
-    @projects = [mock_model(Project).as_new_record.as_null_object]
-    
-  end
-  
   context "it has no list of items" do
     it "should display a message stating there are no items present"
   end
@@ -28,13 +23,44 @@ describe "common/_sortable_list.html.erb" do
       end
     end
     
-    it "should not display the order button"
+    it "should not display the order button" do
+      response.should_not have_selector :span, attribute = {:id => 'order_icon'}
+    end
+
+    it "should not display any of the items handler elements" do
+      response.should_not have_selector :span, attribute = {:class => 'handler'}
+    end
+  end
+
+  context "invalid locals should display an error message if" do
+    it "models variable does not exist" do
+      render :partial => '/common/sortable_list', :locals => {:item_name => 'feature', :assoc => 'story', :order => false}
+      response.should have_selector :span, attribute = {:id => 'error'} do |content|
+        content.should contain "Experiencing problems with displaying the view"
+      end
+    end
+
+    it "item_name does not exist" do
+      render :partial => '/common/sortable_list', :locals => {:models => '', :assoc => 'story', :order => false}
+      response.should have_selector :span, attribute = {:id => 'error'} do |content|
+        content.should contain "Experiencing problems with displaying the view"
+      end
+    end
+
+    it "assoc does not exist" do
+      render :partial => '/common/sortable_list', :locals => {:models => '', :item_name => 'feature', :order => false}
+      response.should have_selector :span, attribute = {:id => 'error'} do |content|
+        content.should contain "Experiencing problems with displaying the view"
+      end
+    end
   end
 
   context "it has a list of items" do
     before(:each) do
-      render :partial => '/common/sortable_list', :locals => {:models => Project.all,  :item_name => 'feature', :assoc => 'story', :order => false}
+      @projects = [mock_model(Project).as_new_record.as_null_object]
+      render :partial => '/common/sortable_list', :locals => {:models => @projects,  :item_name => 'feature', :assoc => 'story', :order => false}
     end
+
     it "should display an icon set for each item" do
       response.should have_selector :ul do |list_item|
         list_item.should have_selector :li, attribute = {:class => 'project'} do |content|
@@ -42,9 +68,8 @@ describe "common/_sortable_list.html.erb" do
         end
       end
     end
-    
-  end
-  
+
+  end  
 
   context "display a sortable list" do
     it "should display the order button"
