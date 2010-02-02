5e66d6c18cca8215d7c521131288e502e6496621
Yomi Colledge <baphled@boodah.net>
2010-02-02 16:04:29 +0000

    Finished specs for implementing our project summary.

--- spec/views/common/_model_summary.html.erb_spec.rb
+++ spec/views/common/_model_summary.html.erb_spec.rb
@@ -5,23 +5,35 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/common/_model_summary.html.erb" do
 
-  context "displaying a newly created projects summary" do
-    before(:each) do
-      @date = Time.now
-      @project = mock_model(Project,
-                              :title => 'A project',
-                              :description => 'A description',
-                              :aim => 'An aim',
-                              :created_at => @date).as_null_object
-      render :locals => {:model => @project, :assoc => :features}
+  before(:each) do
+    @date = Time.now
+    @project = Project.first
+  end
+  describe "a project summary" do
+
+    context "when the project is new" do
+      before(:each) do
+        render :locals => {:model => @project, :assoc => :features}
+      end
+
+      it "should display the created at field" do
+        response.should contain "#{@project.creation_date}"
+      end
+
+      it "should display the date the project was updated" do
+        response.should_not contain "Updated at: #{@project.updated_date}"
+      end
     end
 
-    it "should display the created at field" do
-      response.should contain "#{@project.creation_date}"
-    end
+    context "when the project is updated" do
+      before(:each) do
+        @project.update_attribute(:updated_at, @date.tomorrow)
+        render :locals => {:model => @project, :assoc => :features}
+      end
 
-    it "should display the date the project was updated" do
-      response.should_not contain "Updated at: #{@project.updated_date}"
+      it "should display the date the project was updated" do
+        response.should contain "Updated date: #{@project.updated_date}"
+      end
     end
   end
   
