875a33a5d37b59bf62b9ec66c8c7ced9f836cf29
Yomi Colledge <baphled@boodah.net>
2010-02-15 23:08:03 +0000

    Moved clickable functionality to the form js file where it belongs.

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
+
   $('form').submit(function() {
     var postData = $(this).serialize();
     $.post($(this).attr('action'), postData, null, "script");
     return false;
   });
 
+  $("a.display_form").live("click",function(event) {
+    var link = $(this);
+    var id_attribute_name = link.attr('id').split('_');
+    var title_array = link.text().split(' ');
+
+    $('form').toggle(function() {
+      if('view' == link.text().substr(0,4)) {
+        link.text('hide ' + title_array[1]);
+      } else {
+        link.text('view ' + title_array[1]);
+      }
+    });
+  });
 });
