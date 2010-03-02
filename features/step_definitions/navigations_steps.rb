9fa98b38a7470787fa742626cddaffea93100b09
Yomi Colledge <baphled@boodah.net>
2010-03-02 13:38:10 +0000

    Added step, really need to refactor as the step has duplication

--- features/step_definitions/navigations_steps.rb
+++ features/step_definitions/navigations_steps.rb
@@ -2,6 +2,10 @@ Given /^I can view the "(.*)" page$/ do |controller|
   visit "/#{controller}"
 end
 
+Given /^I can view the (.*) page$/ do |controller|
+  visit "/#{controller}"
+end
+
 #@TODO refactor so it more flexible
 When /^I click (.*) (.*)$/ do |action,controller|
   visit "/#{controller}/#{action}"
