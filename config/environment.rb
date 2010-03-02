a7b3c0e51260561f802971ab1ee0a003589f7e96
Yomi Colledge <baphled@boodah.net>
2010-03-02 11:15:16 +0000

    Added simple navigation for displaying our menus

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   config.gem 'jrails', :source => 'http://gemcutter.org'
   config.gem 'validation_reflection'
   config.gem 'will_paginate', :version => '>=2.3.6'
-  
+  config.gem 'simple-navigation', :lib => 'simple_navigation'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
