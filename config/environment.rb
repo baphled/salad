5d6ad78c106ee02ffad3feaf77eed05346e52f35
Yomi Colledge <baphled@boodah.net>
2010-03-08 17:43:44 +0000

    Added deadweight, to handle our legacy CSS, will add task later.

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem 'validation_reflection'
   config.gem 'will_paginate', :version => '>=2.3.6'
   config.gem 'simple-navigation', :lib => 'simple_navigation'
+  config.gem 'deadweight'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
