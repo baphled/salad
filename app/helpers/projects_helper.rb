dad051f70f52b1c00dab13a941b159cd57829636
Yomi Colledge <baphled@boodah.net>
2010-03-08 22:34:47 +0000

    Refactored.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,25 +1,15 @@
 module ProjectsHelper
   def steps_imported? list
     list.each do |file|
-      if Feature.find_by_title(file[:feature].title).nil?
-        return false
-  	  end
-	  end
+      return (Feature.find_by_title(file[:feature].title).nil?)? false : nil
+    end
   end
 
   def has_no_stories? feature
-    if feature.stories.blank?
-      true
-    else
-      false
-    end
+    (feature.stories.blank?)? true : false
   end
 
   def invalid_import? feature
-    if feature.title.blank? || feature.in_order.blank? ||  feature.as_a.blank? || feature.i_want.blank?
-      true
-    else
-      false
-    end
+    (feature.title.blank? || feature.in_order.blank? ||  feature.as_a.blank? || feature.i_want.blank?)? true : false
   end
 end
