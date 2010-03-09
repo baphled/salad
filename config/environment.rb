7cc42d89697981278eb0ea8df506e164e3f9e876
baphled <baphled@meedihoe.bo0dah>
2010-03-09 23:47:16 +0000

    Added pretty diff which we'll use for displaying the difference between features.

--- config/environment.rb
+++ config/environment.rb
@@ -30,6 +30,8 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate', :version => '>=2.3.6'
   config.gem 'simple-navigation', :lib => 'simple_navigation'
   config.gem 'deadweight'
+  config.gem 'pretty_diff'
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
