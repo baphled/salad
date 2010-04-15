ce481ee6d57551c12e550e1a50c3d68154e82eb3
Yomi Colledge <baphled@boodah.net>
2010-04-15 22:34:00 +0100

    Added missing view file, netbeans seems to do this from time to time.

---
+++ spec/views/features/system_merge.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "features/system_merge.html.erb" do
+
+  describe "GET, system_merge" do
+    before(:each) do
+      @feature = FeatureFile.new("#{RAILS_ROOT}/features/plain/tag_cloud.feature").export
+      File.open("#{RAILS_ROOT}/tmp/tag_cloud.feature", 'w') { |f| f.write(@feature.export) }
+      @feature.update_attribute(:path, "#{RAILS_ROOT}/spec/fixtures/features/tag_cloud.feature")
+      assigns[:feature] = @feature
+      assigns[:pretty] = @feature.diff_reverse
+      render
+    end
+
+    it "should have a link to update the system feature" do
+      response.should have_selector :a, attributes = {:href => update_system_feature_path(@feature)}
+    end
+  end
+end
