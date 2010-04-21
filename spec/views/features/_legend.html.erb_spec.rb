c9b624ceef22a6846d508e3f6f890cbea9d8084c
Yomi Colledge <baphled@boodah.net>
2010-04-21 11:51:27 +0100

    Refactored so that the _legend partial is more flexible, also refactored specs and views to reflect this change.

--- spec/views/features/_legend.html.erb_spec.rb
+++ spec/views/features/_legend.html.erb_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/features/_legend.html.erb" do
   context "display for file changes" do
     before(:each) do
-      render :locals => {:diff_for => "file"}
+      render :locals => {:diff_from => "file", :diff_to => "file"}
     end
     
     it "should have a legend displaying system changes as green" do
@@ -25,7 +25,7 @@ describe "/features/_legend.html.erb" do
   
   context "display for system changes" do
     before(:each) do
-      render :locals => {:diff_for => "system"}
+      render :locals => {:diff_from => "system", :diff_to => "system"}
     end
     
     it "should have a legend displaying system changes as green" do
@@ -47,7 +47,7 @@ describe "/features/_legend.html.erb" do
   
   context "passing a change parameter" do
     before(:each) do
-      render :locals => {:diff_for => "system", :change => true}
+      render :locals => {:diff_from => "system", :diff_to => "file", :change => true}
     end
     it "should display a different copy for changes to the system" do
       response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
@@ -57,6 +57,12 @@ describe "/features/_legend.html.erb" do
       end
     end
     
-    it "should display a different copy for changes to the file"
+    it "should display a different copy for changes to the file" do
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
+        legend_wrapper.should have_selector :span, attribute = {:class => "gi"} do |content|
+          content.should contain "Changes to the file"
+        end
+      end
+    end
   end
 end
