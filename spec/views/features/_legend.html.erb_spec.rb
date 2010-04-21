ac18def63a7f722f40f94401759b4d215358859b
Yomi Colledge <baphled@boodah.net>
2010-04-21 21:30:37 +0100

    Restructured layout.

--- spec/views/features/_legend.html.erb_spec.rb
+++ spec/views/features/_legend.html.erb_spec.rb
@@ -7,7 +7,7 @@ describe "/features/_legend.html.erb" do
     end
     
     it "should have a legend displaying system changes as green" do
-      response.should have_selector :span, attribute = {:id => "legend"} do |legend_wrapper|
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
         legend_wrapper.should have_selector :span, attribute = {:class => "gd"} do |content|
           content.should contain "Removing from file"
         end
@@ -15,7 +15,7 @@ describe "/features/_legend.html.erb" do
     end
 
     it "should have a legend displaying file changes as red" do
-      response.should have_selector :span, attribute = {:id => "legend"} do |legend_wrapper|
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
         legend_wrapper.should have_selector :span, attribute = {:class => "gi"} do |content|
           content.should contain "Adding to file"
         end
@@ -29,7 +29,7 @@ describe "/features/_legend.html.erb" do
     end
     
     it "should have a legend displaying system changes as green" do
-      response.should have_selector :span, attribute = {:id => "legend"} do |legend_wrapper|
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
         legend_wrapper.should have_selector :span, attribute = {:class => "gi"} do |content|
           content.should contain "Adding to system"
         end
@@ -37,7 +37,7 @@ describe "/features/_legend.html.erb" do
     end
 
     it "should have a legend displaying file changes as red" do
-      response.should have_selector :span, attribute = {:id => "legend"} do |legend_wrapper|
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
         legend_wrapper.should have_selector :span, attribute = {:class => "gd"} do |content|
           content.should contain "Removing from system"
         end
@@ -50,7 +50,7 @@ describe "/features/_legend.html.erb" do
       render :locals => {:diff_from => "system", :diff_to => "file", :change => true}
     end
     it "should display a different copy for changes to the system" do
-      response.should have_selector :span, attribute = {:id => "legend"} do |legend_wrapper|
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
         legend_wrapper.should have_selector :span, attribute = {:class => "gd"} do |content|
           content.should contain "Changes to the system"
         end
@@ -58,7 +58,7 @@ describe "/features/_legend.html.erb" do
     end
     
     it "should display a different copy for changes to the file" do
-      response.should have_selector :span, attribute = {:id => "legend"} do |legend_wrapper|
+      response.should have_selector :div, attribute = {:id => "legend"} do |legend_wrapper|
         legend_wrapper.should have_selector :span, attribute = {:class => "gi"} do |content|
           content.should contain "Changes to the file"
         end
