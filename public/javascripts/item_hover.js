993623a2379f8bc6566992645ad9b20e9bec3ee7
Yomi Colledge <baphled@boodah.net>
2010-05-12 17:27:45 +0100

    Refactored so that we handle displaying an sortable items associated items easier.

--- public/javascripts/item_hover.js
+++ public/javascripts/item_hover.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
+  
+  var hoverItem = function() {
+    $classArray = $(this).attr('id');
+    $sidebarPostfix = $('div#sidebar > ul').attr('id').split('_');
+    
+    $hover_class = 'li#' + $classArray + '_' + $sidebarPostfix[1];
+    $($hover_class).toggle();
+  };
+  
   $("div.list_item").each(function(event) {
-
-    $(this).mouseover(function() {
-      $classArray = $(this).attr('class').replace(' ','_').split(' ');
-      $hover_class = 'li.' + $classArray[1];
-      $($hover_class).toggle();
-    });
-
-    $(this).mouseout(function() {
-      $classArray = $(this).attr('class').replace(' ','_').split(' ');
-      $hover_class = 'li.' + $classArray[1];
-      $($hover_class).toggle();
-    });
+    $(this).mouseover(hoverItem);
+    $(this).mouseout(hoverItem);
   });
 });
