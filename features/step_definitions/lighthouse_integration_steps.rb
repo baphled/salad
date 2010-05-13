a90a82314bbeccd6ed6164a670f8731fc410861b
Yomi Colledge <baphled@boodah.net>
2010-05-13 17:25:24 +0100

    Added steps to implement functionality.

--- features/step_definitions/lighthouse_integration_steps.rb
+++ features/step_definitions/lighthouse_integration_steps.rb
@@ -3,9 +3,17 @@ Given /^we are using the account name "([^\"]*)"$/ do |account_name|
 end
 
 Given /^we have an the project number "([^\"]*)"$/ do |project_number|
-  @project = Lighthouse::Project.find(project_number)
+  @project_number = project_number
+end
+
+When /^we specify the ticket type "([^\"]*)"$/ do |ticket_parameters|
+  @lighthouse_tickets = Lighthouse::Ticket.find(:all, :params => { :project_id => @project_number, :q => "state:open tagged:feature" })
 end
 
 When /^we retrieve tickets$/ do
-  @project.tickets.should_not be_empty
+  @lighthouse_tickets.should_not be_empty
+end
+
+Then /^each of the features should be using the "([^\"]*)" tag$/ do |tag|
+  @lighthouse_tickets.each {|ticket| ticket.tag.should contain 'feature'}
 end
