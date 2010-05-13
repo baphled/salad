557522796e9c93dcda5042d19bbe040e5dd23cce
Yomi Colledge <baphled@boodah.net>
2010-05-13 18:03:43 +0100

    Added view specs for the new parking index view.

---
+++ spec/views/parking/index.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe '/parking/index.html' do
+  context "No tickets have been found" do
+    before(:each) do
+      render 'parking/index'
+    end
+    
+    it "should display a form" do
+      response.should have_selector :form
+    end
+    it "should have an input box for the tag name"
+    it "should allow the user to submit the query"
+  end
+  
+  context "tickets have been retrieved" do
+    it "should allow the user to retrieve a list of tickets from lighthouse"
+    it "should have a import button"    
+  end
+end
