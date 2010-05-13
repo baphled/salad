57ae7eeb3fa7f1e1116f6b4a785a375ab756ab79
Yomi Colledge <baphled@boodah.net>
2010-05-13 23:07:14 +0100

    Implemented initial specs to create the basics of form need needed to search for new potential features from lighthouse.

--- spec/views/parking/index.html.erb_spec.rb
+++ spec/views/parking/index.html.erb_spec.rb
@@ -9,8 +9,13 @@ describe '/parking/index.html' do
     it "should display a form" do
       response.should have_selector :form
     end
-    it "should have an input box for the tag name"
-    it "should allow the user to submit the query"
+    it "should have an input box for the tag name" do
+      response.should have_selector :input
+    end
+
+    it "should allow the user to submit the query" do
+      response.should have_selector :button
+    end
   end
   
   context "tickets have been retrieved" do
