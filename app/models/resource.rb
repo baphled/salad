85f7511a6f24258cf22a91ee6904d18a333a20dd
Yomi Colledge <baphled@boodah.net>
2010-05-14 16:02:29 +0100

    Added specs and implementation code for the parking controlers tickets action.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,7 @@
 class Resource < ActiveRecord::Base
+  
+  def self.tickets(user_name, project_id, tag)
+    Lighthouse.account = user_name
+    Lighthouse::Ticket.find(:all, :params => { :project_id => project_id, :q => "state:open tagged:#{tag}" })
+  end
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -5,5 +5,12 @@ describe Resource do
     Resource.stub!(:name).and_return 'baphled'
     Resource.name.should eql 'baphled'
   end
-  it "should have a project name or id"
+  it "should have a project name or id" do
+    Resource.stub!(:project_id).and_return '50164'
+    Resource.project_id.should eql '50164'
+  end
+  
+  it "should be able to make a call to lighthouse" do
+    Resource.tickets('baphled', '50164', 'feature').should_not be_empty
+  end
 end
