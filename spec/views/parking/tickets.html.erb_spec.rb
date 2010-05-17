a3b17a39e701569d785066c56ce3fc15cdff6638
Yomi Colledge <baphled@boodah.net>
2010-05-17 15:19:35 +0100

    Added specs and implementation code for viewing our tickets results.

--- spec/views/parking/tickets.html.erb_spec.rb
+++ spec/views/parking/tickets.html.erb_spec.rb
@@ -5,23 +5,43 @@ describe "/parking/tickets.html" do
   context "a successful search has been requested" do
     before(:each) do
       @resource = Resource.create(:name => 'baphled', :project_id => '50164')
+      assigns[:tickets] = @resource.tickets('feature')
+      render
     end
     
     it "should display a list of tickets" do
-      assigns[:tickets] = @resource.tickets('feature')
-      render
       response.should have_selector :ul
     end
     
     it "should display each ticket's title" do
-      assigns[:tickets] = @resource.tickets('feature')
-      render
       assigns[:tickets].should_not be_empty
     end
     
-    it "should display each ticket's date"
-    it "should display each ticket's tag"
-    it "should display each ticket's description"
+    it "should display each ticket's date" do
+      assigns[:tickets].each do |ticket|
+        ticket.created_at.should_not be_nil
+      end
+    end
+    
+    it "should display each ticket's tag" do
+      assigns[:tickets].each do |ticket|
+        response.should have_selector :ul do |list|
+          list.should have_selector :li do |content|
+            content.should contain ticket.tags.first
+          end
+        end
+      end
+    end
+    
+    it "should display each ticket's description" do
+      assigns[:tickets].each do |ticket|
+        response.should have_selector :ul do |list|
+          list.should have_selector :li do |content|
+            content.should contain ticket.original_body_html
+          end
+        end
+      end
+    end
   end
 
   context "no tickets where found" do
