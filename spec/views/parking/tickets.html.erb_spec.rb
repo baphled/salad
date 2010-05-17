57eb3fe051e68407bae7b4626bc48db2c8ad2dbd
Yomi Colledge <baphled@boodah.net>
2010-05-17 12:26:38 +0100

    Added tickets view and spec.

---
+++ spec/views/parking/tickets.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "/parking/tickets.html" do
+
+  context "a successful search has been requested" do
+    before(:each) do
+      @resource = Resource.new(:name => 'baphled', :project_id => '50164')
+    end
+    
+    it "should display a list of tickets" do
+      assigns[:tickets] = @resource.tickets('feature')
+      render
+      response.should have_selector :ul
+    end
+    
+    it "should display each ticket's title" do
+      assigns[:tickets] = @resource.tickets('feature')
+      render
+      assigns[:tickets].should_not be_empty
+    end
+    
+    it "should display each ticket's date"
+    it "should display each ticket's tag"
+    it "should display each ticket's description"
+  end
+
+  context "no tickets where found" do
+    it "should return an empty list"
+    it "should display a flash message"
+  end
+end
