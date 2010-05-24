a3bd0f026d1ea751e07276756500d23514be0e20
Yomi Colledge <baphled@boodah.net>
2010-05-24 16:44:41 +0100

    Added new ticket method

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,12 @@
 class Resource < ActiveRecord::Base
+  
   def tickets(tag)
     Lighthouse.account = name
     Lighthouse::Ticket.find(:all, :params => { :project_id => project_id, :q => "state:open tagged:#{tag}" })
+  end
+  
+  def ticket(number)
+    Lighthouse.account = self.name
+    Lighthouse::Ticket.find(number, :params => { :project_id => project_id})
   end
 end
