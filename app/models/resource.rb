980dc915b454bd11e433cbac16056c5c76151831
Yomi Colledge <baphled@boodah.net>
2010-05-30 20:39:54 +0100

    Added validations.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,10 @@
 class Resource < ActiveRecord::Base
-  
+  validates_presence_of     :name
+  validates_presence_of     :project_id
+
+  validate :valid_lighthouse_account?, :if => :name_not_empty?
+  validate :valid_lighthouse_project_id?, :if => :project_id_not_empty?
+
   def tickets(tag)
     Lighthouse.account = name
     Lighthouse::Ticket.find(:all, :params => { :project_id => project_id, :q => "state:open tagged:#{tag}" })
@@ -9,4 +14,31 @@ class Resource < ActiveRecord::Base
     Lighthouse.account = self.name
     Lighthouse::Ticket.find(number, :params => { :project_id => project_id})
   end
+
+  def valid_lighthouse_account?
+    begin
+      Lighthouse.account = self.name
+      Lighthouse::Project.find :all
+    rescue ActiveResource::UnauthorizedAccess
+      errors.add(:name, "Must be a valid LightHouse project name")
+    end
+  end
+
+  def valid_lighthouse_project_id?
+    begin
+      Lighthouse.account = self.name
+      Lighthouse::Project.find self.project_id
+    rescue ActiveResource::ResourceNotFound
+      errors.add(:project_id, "Must be a valid LightHouse project id")
+    end
+  end
+  
+  private
+    def name_not_empty?
+      (self.name == '')? false : true
+    end
+
+    def project_id_not_empty?
+      (self.project_id  == '' or self.name == '')? false : true
+    end
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -14,4 +14,31 @@ describe Resource do
     @resource = Resource.new(:name => 'baphled', :project_id => '50164')
     @resource.tickets('feature').should_not be_empty
   end
+
+  context "invalid resource" do
+    before(:each) do
+      @resource = Resource.new(:name => '', :project_id => '')
+    end
+    it "should have a project name" do
+      @resource.save
+      @resource.errors[:name].should eql "can't be blank"
+    end
+
+    it "should have a project id" do
+      @resource.save
+      @resource.errors[:project_id].should eql "can't be blank"
+    end
+
+    it "should have a valid project name" do
+      @resource = Resource.new(:name => 'foo', :project_id => 23)
+      @resource.save
+      @resource.errors[:name].should eql "Must be a valid LightHouse project name"
+    end
+
+    it "should have a valid project id" do
+      @resource = Resource.new(:name => 'baphled', :project_id => 23)
+      @resource.save
+      @resource.errors[:project_id].should eql "Must be a valid LightHouse project id"
+    end
+  end
 end
