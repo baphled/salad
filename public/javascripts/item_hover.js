b87a60ae8afa5245f2e9dff2dd64fcee1211c835
Yomi Colledge <baphled@boodah.net>
2010-06-02 00:07:24 +0100

    General cleanup of code. still need to improve on functionality

--- public/javascripts/item_hover.js
+++ public/javascripts/item_hover.js
@@ -2,40 +2,43 @@ $(document).ready(function() {
 
   $.fn.autoScroller = function(speed,height) {
     var $innerUnorderedList = $(this).find('ul');
-    console.log($innerUnorderedList);
     $innerUnorderedList.css('height', height)
                        .css('overflow-y', 'auto');
 
-    $innerUnorderedList.scrollTop(0);
-    var scrollList = setInterval(function() {
-      var previousOffset = $innerUnorderedList.scrollTop();
 
-      $innerUnorderedList.scrollTop($innerUnorderedList.scrollTop() + 2);
+    if ($(this).css('display') != 'none') {
+      $innerUnorderedList.scrollTop(0);
+      
+      setInterval(function() {
+        var previousOffset = $innerUnorderedList.scrollTop();
 
-      if ($innerUnorderedList.scrollTop() == previousOffset) {
-        $innerUnorderedList.scrollTop(0);
-      }
-    }, speed);
+        $innerUnorderedList.scrollTop($innerUnorderedList.scrollTop() + 2);
+
+        if ($innerUnorderedList.scrollTop() == previousOffset) {
+          $innerUnorderedList.scrollTop(0);
+        }
+      }, speed);
+    }
   }
   
   var $hoverItem = function() {
     var sidebarPostfix = $('div#sidebar > ul').attr('id').split('_');
     var $sidebar = $('div#sidebar > ul');
-    var item = 'li#' + $(this).attr('id') + '_' + sidebarPostfix[1];
+    var hoverItem = 'li#' + $(this).attr('id') + '_' + sidebarPostfix[1];
     var height = $('div#content').height();
 
     height = height - ($('div#footer').height() * 2);
     
-    $sidebar.find(item).toggle();
+    $sidebar.find(hoverItem).toggle();
 
+    $(hoverItem).autoScroller(500, height);
     // Sets the hover item to a fixed position when the cursor is moved
     $(window).scroll(function() {
       var y = $(window).scrollTop();
       if (y >= $('div#sidebar').offset().top) {      
         $sidebar.addClass('fixed');
-        $(item).autoScroller(500, height);
       } else {
-        $(item).css('').find('ul').css('');
+        $(hoverItem).find('ul').css('');
         $sidebar.removeClass('fixed');
       }
     });
