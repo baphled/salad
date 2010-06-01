04d07412d97fd0cf0713d60c65024b535c8d6c11
Yomi Colledge <baphled@boodah.net>
2010-06-01 23:31:15 +0100

    Implemented basic functionality to make the hover items scroll down when the height is within a given limit.
    Still need to iron the functionality out

--- public/javascripts/item_hover.js
+++ public/javascripts/item_hover.js
@@ -1,12 +1,45 @@
 $(document).ready(function() {
+
+  $.fn.autoScroller = function(speed,height) {
+    var $innerUnorderedList = $(this).find('ul');
+    console.log($innerUnorderedList);
+    $innerUnorderedList.css('height', height)
+                       .css('overflow-y', 'auto');
+
+    $innerUnorderedList.scrollTop(0);
+    var scrollList = setInterval(function() {
+      var previousOffset = $innerUnorderedList.scrollTop();
+
+      $innerUnorderedList.scrollTop($innerUnorderedList.scrollTop() + 2);
+
+      if ($innerUnorderedList.scrollTop() == previousOffset) {
+        $innerUnorderedList.scrollTop(0);
+      }
+    }, speed);
+  }
   
   var $hoverItem = function() {
     var sidebarPostfix = $('div#sidebar > ul').attr('id').split('_');
     var $sidebar = $('div#sidebar > ul');
-    
     var item = 'li#' + $(this).attr('id') + '_' + sidebarPostfix[1];
+    var height = $('div#content').height();
+
+    height = height - ($('div#footer').height() * 2);
     
     $sidebar.find(item).toggle();
+
+    // Sets the hover item to a fixed position when the cursor is moved
+    $(window).scroll(function() {
+      var y = $(window).scrollTop();
+      if (y >= $('div#sidebar').offset().top) {      
+        $sidebar.addClass('fixed');
+        $(item).autoScroller(500, height);
+      } else {
+        $(item).css('').find('ul').css('');
+        $sidebar.removeClass('fixed');
+      }
+    });
+
   };
   
   $("div.list_item").live('mouseover mouseout', $hoverItem);
