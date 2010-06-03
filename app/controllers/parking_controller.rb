e835ecf49195ce26c07ff53c438ccaf097f40b51
Yomi Colledge <baphled@boodah.net>
2010-06-03 20:23:27 +0100

    Bad habits from work

--- app/controllers/parking_controller.rb
+++ app/controllers/parking_controller.rb
@@ -31,7 +31,6 @@ class ParkingController < ActionController::Base
     @resource = Resource.find(params[:resource][:id])
     params[:lighthouse][:ticket_id].each do |item|
       ticket = @resource.ticket(item)
-      puts ticket.title
       if not LightHouse.create(:ticket_id => item, :title => ticket.title, :body => ticket.original_body)
         flash[:error] = "Unable to save tickets"
         redirect_to parking_index_path and return
