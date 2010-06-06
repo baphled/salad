0615a568eea51dea1988474613b42932732e05e3
Yomi Colledge <baphled@boodah.net>
2010-06-06 16:45:32 +0100

    Refactored to make out hover item a jquery function

--- public/javascripts/item_hover.js
+++ public/javascripts/item_hover.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $.fn.autoScroller = function(speed,height) {
     var width = $(this).parent().width();
     var $innerUnorderedList = $(this).find('ul');
+    
     $innerUnorderedList.css('max-height', height)
                        .css('overflow-y', 'hidden');
 
@@ -22,33 +23,36 @@ $(document).ready(function() {
       }, speed);
     }
   }
-  
-  var hoverItem = function() {
-    var sidebarPostfix = $('div#sidebar > ul').attr('id').split('_');
-    var $sidebar = $('div#sidebar > ul');
-    var hoverItem = 'li#' + $(this).attr('id') + '_' + sidebarPostfix[1];
-    var height = $('div#lists').outerHeight() - $('div#footer').outerHeight();
 
-    $sidebar.addClass('ui-widget ui-widget-content ui-corner-all');
-    if (!$(hoverItem).is(':visible')) {
-      $('div#sidebar').find('li.side_hover').hide();
-      $sidebar.find(hoverItem).toggle();
-      $(hoverItem).autoScroller(500, height);
-      // Sets the hover item to a fixed position when the cursor is moved
-      $(window).scroll(function() {
-        var y = $(window).scrollTop();
+  $.fn.hoverItem = function() {
+    $(this).live('mouseover', function() {
+      var sidebarPostfix = $('div#sidebar > ul').attr('id').split('_');
+      var $sidebar = $('div#sidebar > ul');
+      var hoverItem = 'li#' + $(this).attr('id') + '_' + sidebarPostfix[1];
+      var height = $('div#lists').outerHeight() - $('div#footer').outerHeight();
 
-        if (y >= $('div#sidebar').offset().top) {
-          $sidebar.css('width', $('.hover').width())
-          $sidebar.addClass('fixed');
-        } else {
-          $(hoverItem).find('ul').css('');
-          $sidebar.removeClass('fixed');
-        }
-      });
-    }
+      $sidebar.addClass('ui-widget ui-widget-content ui-corner-all');
+      
+      if (!$(hoverItem).is(':visible')) {
+        $('div#sidebar').find('li.side_hover').hide();
+        $sidebar.find(hoverItem).toggle();
+        $(hoverItem).autoScroller(500, height);
 
+        $(window).scroll(function() {
+          var y = $(window).scrollTop();
+
+          if (y >= $('div#sidebar').offset().top) {
+            $sidebar.css('width', $('.hover').width())
+            $sidebar.addClass('fixed');
+          } else {
+            $(hoverItem).find('ul').css('');
+            $sidebar.removeClass('fixed');
+          }
+        });
+      }
+    });
   };
-  
-  $("div.list_item").live('mouseover', hoverItem);
+
+
+$("div.list_item").hoverItem();
 });
