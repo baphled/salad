20f267372fb0e04b24a270f46e5fe619812b9df7
Yomi Colledge <baphled@boodah.net>
2010-06-09 11:31:39 +0100

    General cleanup of code base.

--- public/javascripts/scrollingHoverable.js
+++ public/javascripts/scrollingHoverable.js
@@ -5,29 +5,33 @@ $.fn.scrollingHoverable = function(options) {
 
   $(this).live('mouseover', function() {
     var sidebarPostfix = $('div#sidebar > ul').attr('id').split('_');
-    var $sidebar = $('div#sidebar > ul');
+    var $sidebarList = $('div#sidebar > ul');
     var hoverItem = 'li#' + $(this).attr('id') + '_' + sidebarPostfix[1];
-    var height = $('div#lists').outerHeight() - $('div#footer').outerHeight();
     
+    var headerHeight = $(hoverItem).find('h3').outerHeight(),
+        footerHeight = $('div#footer').outerHeight(),
+        listHeight = $('div#lists').outerHeight();
+    
+    var height = (listHeight - footerHeight) + headerHeight;
     var setHoverPosition = function() {
       var y = $(window).scrollTop();
       
       if (y >= $('div#sidebar').offset().top) {
-        $sidebar.css('width', $('.hover').width())
-        $sidebar.addClass('fixed');
+        $sidebarList.css('width', $('.hover').width())
+        $sidebarList.addClass('fixed');
       } else {
-        $(hoverItem).find('ul').css('');
-        $sidebar.removeClass('fixed');
+        // $(hoverItem).find('ul').css('');
+        $sidebarList.removeClass('fixed');
       }
     }
     
-    $sidebar.addClass('ui-widget ui-widget-content ui-corner-all');
+    $sidebarList.addClass('ui-widget ui-widget-content ui-corner-all');
     setHoverPosition();
     
     if (!$(hoverItem).is(':visible')) {
       $('div#sidebar').find('li.side_hover').hide();
-      $sidebar.find(hoverItem).toggle();
-      $(hoverItem).autoScroller({speed: 500, height: height});
+      $sidebarList.find(hoverItem).toggle();
+      $(hoverItem).autoScroller({speed: 200, height: height, stopOnHover: opts.stopOnHover});
 
       $(window).scroll(setHoverPosition);
     }
@@ -44,7 +48,8 @@ $.fn.scrollingHoverable = function(options) {
 // default options
 $.fn.scrollingHoverable.defaults = {
   scrollSpeed:500,
-  height:500
+  height:500,
+  stopOnHover: false
 };
 
 })(jQuery);
