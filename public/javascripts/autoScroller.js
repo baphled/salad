37bd4c5a3be63c25a4f2d3844e4f9da1f88746d3
Yomi Colledge <baphled@boodah.net>
2010-06-11 15:04:46 +0100

    Improved how the previous offset is stored.

--- public/javascripts/autoScroller.js
+++ public/javascripts/autoScroller.js
@@ -10,26 +10,25 @@ $.fn.autoScroller = function(options) {
   var opts = $.extend({}, $.fn.autoScroller.defaults, options);
   var width = $(this).parent().width();
   var $innerUnorderedList = $(this).find('ul');
-  
+  var previousOffset = 0;
+
   $innerUnorderedList.css('max-height', opts.height)
                      .css('overflow-y', 'hidden');
 
   $(this).parent().css('width', width);
-  
+
   if ($(this).is(':visible')) {
     var stop = false;
     $innerUnorderedList.scrollTop(0);
-    
+
     setInterval(function() {
       if (opts.stopOnHover) {
         $('div#sidebar > ul').live('mouseout mouseover', function(event) {
           stop = (event.type == 'mouseover')? true : false;
-        });        
+        });
       };
 
       if (false == stop) {
-        var previousOffset = $innerUnorderedList.scrollTop();
-
         $innerUnorderedList.scrollTop($innerUnorderedList.scrollTop() + 1);
 
         if ($innerUnorderedList.scrollTop() == previousOffset && 0 !== previousOffset) {
@@ -38,6 +37,7 @@ $.fn.autoScroller = function(options) {
               $(this).scrollTop(0).animate({opacity: 1}, 300);
             });
         }
+        previousOffset = $innerUnorderedList.scrollTop();
       }
     }, opts.speed);
   }
