98b360d64f8d7a96cb31f49c84611f335f67b383
Yomi Colledge <baphled@boodah.net>
2010-06-15 15:13:59 +0100

    Refactored to be called multiple times, will need to make a call to this plugin each time an AJAX call is made

--- public/javascripts/jquery-icon-animation.js
+++ public/javascripts/jquery-icon-animation.js
@@ -1,21 +1,34 @@
-$(function() {
-  // icon animation
-  var $iconList = $('span.icons >ul'),
-      $innerWrapper = $('<span>')
-        .addClass('ui-icon ui-icon-info')
-        .css({'float': 'left', 'margin-right': '0.3em'}),
-      $innerContent = $('<strong class="dbl-click">Double click</strong>')
-        .append(' to view panel'),
-      $textWrapper = $('<span>')
-        .append($innerWrapper)
-        .append($innerContent)
-        .addClass('view-panel ui-state-highlight')
-        .css({'display': 'block', 'float': 'right'});
+(function($) {
+  /**
+  * Plugin to help determine the position of a given element, making sure that it is always at the top of the page.
+  *
+  * @useage $('div ul').autoScroller({scrollSpeed: 500, height: 500, stopOnHover: true})
+  *
+  * @author Yomi Colledge
+  **/
+$.fn.animateIconPanel = function(options) {
+  var opts = $.extend({}, $.fn.autoScroller.defaults, options);
   
-  $iconList.hide().parent().removeClass('icons ui-widget ui-widget-content ui-corner-all');
-  $iconList.before($textWrapper);
+  $(this).each(function() {
+    var $iconList = $('ul.icons', this),
+          $innerWrapper = $('<span>')
+            .addClass('ui-icon ui-icon-info')
+            .css({'float': 'left', 'margin-right': '0.3em'}),
+          $innerContent = $('<strong class="dbl-click">Double click</strong>')
+            .append(' to view panel'),
+          $textWrapper = $('<span>')
+            .append($innerWrapper)
+            .append($innerContent)
+            .addClass('view-panel ui-state-highlight')
+            .css({'display': 'block', 'float': 'right'});
 
-  $('span#panel, ul.items-list li > span').live('dblclick', function(event) {
+    $iconList.hide().parent().removeClass('icons ui-widget ui-widget-content ui-corner-all');
+    $iconList.before($textWrapper);
+  })
+
+  $(this).unbind('dbclick');
+  
+  $(this).bind('dblclick', function(event) {
     var $iconsWrapper = $('ul', this),
         $openLink = $iconsWrapper.parent().find('>span'),
         $panel = $(this);
@@ -28,10 +41,23 @@ $(function() {
       });
     } else {
       $iconsWrapper.animate({opacity: 'toggle', height: 'toggle', width: 'toggle', background: '#89A407'}, 'slow', function() {
-        $panel.removeClass('icons ui-widget ui-widget-content ui-corner-all');
+      $panel.removeClass('icons ui-widget ui-widget-content ui-corner-all');
         $openLink.fadeIn();
       });
     }
   });
 
-})
+  // private function for debugging
+  function debug($obj) {
+    if (window.console && window.console.log) {
+      window.console.log($obj);
+    }
+  }
+};
+
+// default options
+$.fn.animateIconPanel.defaults = {
+  speed:500
+};
+
+})(jQuery);
