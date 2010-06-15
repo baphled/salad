75cc75de173b876bc73787b0e5aa9eccc0b92462
Yomi Colledge <baphled@boodah.net>
2010-06-15 02:17:09 +0100

    General cleanup of code.

--- public/javascripts/jquery-icon-animation.js
+++ public/javascripts/jquery-icon-animation.js
@@ -1,10 +1,19 @@
 $(function() {
   // icon animation
-  var $iconList = $('span.icons >ul');
-
+  var $iconList = $('span.icons >ul'),
+      $innerWrapper = $('<span>')
+        .addClass('ui-icon ui-icon-info')
+        .css({'float': 'left', 'margin-right': '0.3em'}),
+      $innerContent = $('<strong class="dbl-click">Double click</strong>')
+        .append(' to view panel'),
+      $textWrapper = $('<span>')
+        .append($innerWrapper)
+        .append($innerContent)
+        .addClass('view-panel ui-state-highlight')
+        .css({'display': 'block', 'float': 'right'});
+  
   $iconList.hide().parent().removeClass('icons ui-widget ui-widget-content ui-corner-all');
-  $iconList.before('<span class="view-panel">Double click to view panel</span>');
-  $('span.view-panel').css({'display': 'block', 'float': 'right'});
+  $iconList.before($textWrapper);
 
   $('span#panel, ul.items-list li > span').live('dblclick', function(event) {
     var $iconsWrapper = $('ul', this),
@@ -18,10 +27,10 @@ $(function() {
         $iconsWrapper.animate({opacity: 'toggle', height: 'toggle', width: 'toggle', background: '#fff'}, 'slow');
       });
     } else {
+      $panel.removeClass('icons ui-widget ui-widget-content ui-corner-all');
       $iconsWrapper.animate({opacity: 'toggle', height: 'toggle', width: 'toggle', background: '#89A407'}, 'slow', function() {
-        $panel.removeClass('icons ui-widget ui-widget-content ui-corner-all');
+        $openLink.fadeIn();
       });
-      $openLink.show();
     }
   });
 
