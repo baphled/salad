3d659afd32a43287db9609e529ebbd5addb0039b
Yomi Colledge <baphled@boodah.net>
2010-06-15 01:02:03 +0100

    Added basic functionality to allow a user to double click on the view icons text to display the panel icons.

---
+++ public/javascripts/jquery-icon-animation.js
@@ -0,0 +1,29 @@
+$(function() {
+  // icon animation
+  var $iconList = $('span.icons ul');
+
+  $iconList.hide().parent().removeClass('icons ui-widget ui-widget-content ui-corner-all');
+  $iconList.before('<span>Double click to view panel</span>');
+
+  $('span#panel').live('dblclick', function(event) {
+    console.log(event.target);
+    
+    var $iconsWrapper = $('ul', this),
+        $openLink = $iconsWrapper.parent().find('>span'),
+        $panel = $(this);
+
+    // is list visible
+    if ($iconsWrapper.is(':visible') == false) {
+      $openLink.fadeOut(300, function() {
+        $panel.addClass('icons ui-widget ui-widget-content ui-corner-all');
+        $iconsWrapper.animate({opacity: 'toggle', height: 'toggle', width: 'toggle', background: '#fff'}, 'slow');
+      });
+    } else {
+      $iconsWrapper.animate({opacity: 'toggle', height: 'toggle', width: 'toggle', background: '#89A407'}, 'slow', function() {
+        $panel.removeClass('icons ui-widget ui-widget-content ui-corner-all');
+      });
+      $openLink.show();
+    }
+  });
+
+})
