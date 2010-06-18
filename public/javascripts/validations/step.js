808cd031089feb0bdb8299502598c376150efee9
Yomi Colledge <baphled@boodah.net>
2010-06-18 01:26:21 +0100

    Added custom properties to improve the layout of our form validations, Will need to refactor these so that these settings  are centralised at a later stage.

--- public/javascripts/validations/step.js
+++ public/javascripts/validations/step.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
   $('form.step').validate({
+    highlight: function(element, errorClass) {
+     $(element).fadeOut(function() {
+       $(element).fadeIn();
+       $(element).addClass(errorClass);
+     });
+    },
+    unhighlight: function(element, errorClass) {
+      var $parent = $(element).parent();
+      $parent.find('span.error').hide();
+    },
+    successClass: 'valid',
+    errorClass: 'error ui-state-error',
+    errorElement: "span",
+    errorPlacement: function(error, element) {
+      var $warningSpan = $('<span></span')
+                        .css({'float': 'left', 'margin': '0.3em'})
+                        .addClass('ui-icon ui-icon-alert'),
+          $parent = element.parent(),
+          $previousError = $parent.find('span.error');
+
+      if ($previousError.html() == null) {
+        error
+          .wrap("<span class='ui-state-error ui-corner-all'></span>")
+          .insertAfter(element)
+          .prepend($warningSpan);
+      } else {
+        $previousError.show();
+      }
+    },
     rules: {
       "step[title]": {
         required: true,
