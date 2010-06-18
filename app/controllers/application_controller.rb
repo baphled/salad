ac2c15894cc4ee9acc94c9a0a25fc49990674a47
Yomi Colledge <baphled@boodah.net>
2010-06-18 22:22:29 +0100

    Refactored to get spec to pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,6 @@ class ApplicationController < ActionController::Base
 	end
 
   def default_or_defined
-    (action_name == 'new')? 'no_sidebar' : 'application'
+    ("new, create".include? action_name)? 'no_sidebar' : 'application'
   end
 end
