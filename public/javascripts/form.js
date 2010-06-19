45a52e95f80be89131cdefb891a4c162faeaa517
Yomi Colledge <baphled@boodah.net>
2010-06-19 13:47:47 +0100

    Refactored so that our customed validations are stored in one place need to refactor the rest of the forms to use the new customised plugin

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -20,5 +20,57 @@ $(document).ready(function() {
     });
   });
 
+  $.fn.customValidations = function(options) {
+    var opts = $.extend({}, $.fn.animateIconPanel.defaults, options);
+
+    $(this).validate({
+      ignoreTitle: true,
+      highlight: function(element, errorClass, validClass) {
+       $(element).addClass(errorClass).removeClass(validClass);
+       $(element).fadeOut(function() {
+         var $parent = $(element).parent();
+         var $validElements = $parent.find('span.valid');
+         $validElements.remove();
+
+         $parent.addClass(errorClass).removeClass(validClass);
+         $(element).fadeIn();
+       });
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        var $parent = $(element).parent();
+        $(element).removeClass(errorClass).addClass(validClass);
+        $parent.removeClass(errorClass).addClass(validClass);
+      },
+      success: function(label) {
+        console.log(label);
+        var $parent = $(label).parent();
+  //      console.log($parent.find('span'));
+        var $validElements = $parent.find('span.valid');
+        $(label).html();
+        $validElements.remove();
+        label.removeClass('error ui-state-error').addClass("valid");
+      },
+      successClass: 'valid',
+      errorClass: 'error ui-state-error',
+      errorElement: "span",
+      errorPlacement: function(error, element) {
+        var $warningSpan = $('<span></span')
+                          .css({'float': 'left', 'margin': '0.3em'})
+                          .addClass('ui-icon ui-icon-alert'),
+            $parent = element.parent(),
+            $previousError = $parent.find('span.error');
+
+        if ($previousError.html() == null) {
+          error
+            .wrap("<span class='ui-state-error ui-corner-all'></span>")
+            .insertAfter(element)
+            .prepend($warningSpan);
+        } else {
+          $previousError.show();
+        }
+      },
+      rules: opts.rules
+    });
+  }
   $('fieldset.inputs > ol li').generateFormatasticTooltips();
 });
--- public/javascripts/validations/project.js
+++ public/javascripts/validations/project.js
@@ -1,34 +1,5 @@
 $(document).ready(function () {
-  $("form.project").validate({
-    highlight: function(element, errorClass) {
-     $(element).fadeOut(function() {
-       $(element).fadeIn();
-       $(element).addClass(errorClass);
-     });
-    },
-    unhighlight: function(element, errorClass) {
-      var $parent = $(element).parent();
-      $parent.find('span.error').hide();
-    },
-    successClass: 'valid',
-    errorClass: 'error ui-state-error',
-    errorElement: "span",
-    errorPlacement: function(error, element) {
-      var $warningSpan = $('<span></span')
-                        .css({'float': 'left', 'margin': '0.3em'})
-                        .addClass('ui-icon ui-icon-alert'),
-          $parent = element.parent(),
-          $previousError = $parent.find('span.error');
-
-      if ($previousError.html() == null) {
-        error
-          .wrap("<span class='ui-state-error ui-corner-all'></span>")
-          .insertAfter(element)
-          .prepend($warningSpan);
-      } else {
-        $previousError.show();
-      }
-    },
+  $("form.project").customValidations({
     rules: {
       "project[title]": {required: true, minlength: 3,
         remote: {
@@ -58,5 +29,4 @@ $(document).ready(function () {
       }
     }
   });
-
 });
