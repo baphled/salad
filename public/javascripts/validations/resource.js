0f64b346abd651c0e67a8e13b6ae328d5a6e9cb1
Yomi Colledge <baphled@boodah.net>
2010-06-20 20:37:38 +0100

    Added client side validations

--- public/javascripts/validations/resource.js
+++ public/javascripts/validations/resource.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
   $('form.resource').customValidations({
     rules: {
-      "resource[name]": {required: true, minlength: 3},
+      "resource[name]": {
+        required: true,
+        minlength: 3,
+        remote: {
+          url: "/parking/unique_resource_name.json",
+          type: "get",
+          data: {
+            "name": function() {
+              return $('#resource_name').val();
+            }
+          }
+        }
+      },
       "resource[project]": {required: true, minlength: 3}
     }
   });
