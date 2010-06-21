0bc517fbb8a76da59c8bbe925e35f9113b9d1b9f
Yomi Colledge <baphled@boodah.net>
2010-06-21 15:10:31 +0100

    Refactored to improve the layout and hide the fade elements if they are not needed

--- public/javascripts/autoScroller.js
+++ public/javascripts/autoScroller.js
@@ -30,24 +30,26 @@ $.fn.autoScroller = function(options) {
     $innerUnorderedList.scrollTop(0);
     $("#item-list-top").remove();
     $("#item-list-bottom").remove();
-    $('ul.hover li:visible > hr:first').before("<div id=item-list-top />");
-    $('ul.hover li:visible > hr:last').before("<div id=item-list-bottom />");
-    $('body').mouseout(function() {stop = true;}).mouseover(function() {stop = false;});
+    if (parseInt($('ul.hover li:visible').height()) >= 250) {
+      $('ul.hover li:visible > hr:first').before("<div id=item-list-top />");
+      $('ul.hover li:visible > hr:last').before("<div id=item-list-bottom />");
+      $('body').mouseout(function() {stop = true;}).mouseover(function() {stop = false;});
+
+      setInterval(function() {
+
+        if (false == stop) {
+          if ($innerUnorderedList.scrollTop() == previousOffset && null != previousOffset) {
+            $innerUnorderedList.animate({scrollTop: 0}, 'slow');
+            previousOffset = null;
+          } else {
+            $innerUnorderedList.animate({scrollTop: $innerUnorderedList.scrollTop() + 2}, 5);
+          }
+
+          previousOffset = $innerUnorderedList.scrollTop();
     
-    setInterval(function() {
-
-      if (false == stop) {
-        if ($innerUnorderedList.scrollTop() == previousOffset && null != previousOffset) {
-          $innerUnorderedList.animate({scrollTop: 0}, 'slow');
-          previousOffset = null;
-        } else {
-          $innerUnorderedList.animate({scrollTop: $innerUnorderedList.scrollTop() + 2}, 5);
         }
-
-        previousOffset = $innerUnorderedList.scrollTop();
-        
-      }
-    }, opts.speed);
+      }, opts.speed);
+    };
   }
 
   // private function for debugging
