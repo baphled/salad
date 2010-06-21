7f3c5468dfe314cf5bfbb258f196bf543a9682f1
Yomi Colledge <baphled@boodah.net>
2010-06-21 16:12:09 +0100

    bugfix: Rectified typo's

--- public/javascripts/hover_dialog.js
+++ public/javascripts/hover_dialog.js
@@ -2,29 +2,20 @@
 $(document).ready(function() {
   
   // Create acessors for our hover dialog
-  var $formAction = $('form').attr('action');
-  var $formID = $('form').attr('id');
-  
-  // We need singular & plural resource names to take advantage of hover dialog
-  var formIdArray = $formID.split('_');
-  var $resourceSingular = formIdArray[1];
-  var $resourcePlural = formIdArray[1].pluralize();
-  
-  // Selector for our tag input
-  var $tagInput = $('input#'+ $resourceSingular + '_tag_list');
-  var $tagInputWrapper = $('li#'+ $resourceSingular + '_tag_list_input');
-  
-  // The hover dialog we'll use to populate our tags
-  var $hoverDialog = $("<div></div>")
-      .addClass('hover')
-      .addClass('ui-widget')
-      .addClass('ui-widget-content')
-      .addClass('ui-corner-all')
+  var $formAction = $('form').attr('action'),
+      $formID = $('form').attr('id'),
+      formIdArray = $formID.split('_');
+      resourceSingular = formIdArray[1],
+      resourcePlural = resourceSingular.pluralize(),
+      $tagInput = $('input#'+ resourceSingular + '_tag_list'),
+      $tagInputWrapper = $('li#'+ resourceSingular + '_tag_list_input'),
+      $hoverDialog = $("<div></div>")
+      .addClass('hover ui-widget ui-widget ui-corner-all')
       .insertAfter($tagInput)
       .hide();
 
   $tagInput.focus(function() {
-    $url = '/' + $resourcePlural + "/tags.json";
+    $url = '/' + resourcePlural + "/tags.json";
     $.ajax({
     	'url': $url,
         'data': $tagInput.serialize(),
