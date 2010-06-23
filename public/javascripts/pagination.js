d3106259f7596367ea8ba9fce0be866b97bd9572
Yomi Colledge <baphled@boodah.net>
2010-06-23 12:35:22 +0100

    Refactored the pagination script to call our tooltip and animateIconPanel plugins once the results have been returned.

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,13 @@
 $(function() {
 	$(".pagination a").live("click", function(){
 		$(".pagination").html("Page loading...");
-		$.get(this.href, null, null, "script");
+		$.get(this.href, null, function() {
+		  // All pagination data needs the following calls so we'll include them here instead
+		  $('ul.items-list li > span').animateIconPanel({eventType: 'click', eventText: 'Click'});
+      $('a[title]').tipsy({fade: true});
+		}, "script");
 		return false;
 	});
+	
+	
 });
