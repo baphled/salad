6ddecd41f10742137fc2dee7df5386e6b6d081e7
Yomi Colledge <baphled@boodah.net>
2010-06-25 11:36:38 +0100

    Refactored the form submit functionality to handle all the generic functionality for our forms

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -2,9 +2,23 @@ $(document).ready(function() {
 
   $('form').submit(function() {
     var postData = $(this).serialize();
+    var $form = $(this).clone();
+    
+    // Convert our content-wide id to content, to allow us to render the side bar
+    var $content = $('div#container').find('div#content-wide');
+    $content.attr('id', 'content');
+    $("<div id='sidebar'></div>").insertAfter($content);
+  
+    var projectInfo = $("<div></div>").attr('id','info');
+    $('#wrapper').html($(projectInfo));
+    
     $.post($(this).attr('action'), postData, function(data) {
       // here is where we make our necessary form response for all our AJAX based responses
+      $('#wrapper').append($form.hide());
+      $('<a class="display_form" href="javascript://">view form</a>').insertBefore('#wrapper form')
+        .addClass('ui-widget ui-widget-content ui-corner-all');
       
+      // setup our generic list events
       $('ul.items-list li > span').animateIconPanel({eventType: 'click', eventText: 'Click'});
       $('a[title]').tipsy({fade: true});
     }, "script");
