b2011496113b97c548bc9e37f304dd16864befab
Yomi Colledge <baphled@boodah.net>
2010-06-25 12:16:25 +0100

    General cleanup along with setting up a focusout event to hide the hover dialog

--- public/javascripts/hover_dialog.js
+++ public/javascripts/hover_dialog.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
   
   // Create acessors for our hover dialog
   var $formAction = $('form').attr('action'),
-      $formID = $('form').attr('id'),
-      formIdArray = $formID.split('_');
+      formIdArray = $('form').attr('id').split('_');
       resourceSingular = formIdArray[1],
       resourcePlural = resourceSingular.pluralize(),
       $tagInput = $('input#'+ resourceSingular + '_tag_list'),
@@ -17,30 +16,29 @@ $(document).ready(function() {
   $tagInput.focus(function() {
     $url = '/' + resourcePlural + "/tags.json";
     $.ajax({
-    	'url': $url,
-        'data': $tagInput.serialize(),
-    	'dataType': 'json',
-    	'type': 'GET',
-    	'success': function(data) {
-    		if (data.length) {
-    		  if($hoverDialog.empty()) {
-      			$.each(data, $enableHoverLink);
-    		  }
+      url: $url,
+      data: $tagInput.serialize(),
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+    		if (data.length && $hoverDialog.empty()) {
+    			$.each(data, $enableHoverLink);
     		}
     	}
     });
   });
 
-  $tagInputWrapper.blur(function() {
+  $tagInputWrapper.focusout(function() {
     $('.hover').fadeOut();
   });
   
   var $enableHoverLink = function(index, item) {
-	  var tags = $tagInput.val().split(', ');
-	  var result = $.inArray(item['tag']['name'], tags);
+	  var tags = $tagInput.val().split(', '),
+	      result = $.inArray(item['tag']['name'], tags);
 
 	  if (result == -1) {
-		  $hoverDialog.append($('<a href="javascript:///>"')
+		  $hoverDialog
+		    .append($('<a href="javascript:///>"')
 		    .append(item['tag']['name'])
 		    .addClass('hover_select')
 		    .attr('id','item_' + item['tag']['id'])
