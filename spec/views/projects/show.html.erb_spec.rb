e712ae4a06499e6ede8925c64c36558f5bc926c9
Yomi Colledge <baphled@boodah.net>
2010-06-29 10:50:12 +0100

    Added specs for hiding the import buttons when there are no imports available. For one reason or another or scenario for the import button is not working. Will need to resolve this ASAP.

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -45,6 +45,10 @@ describe "projects/show.html.erb" do
     it "should not have an import link" do
       response.should_not have_selector :a, attribute = {:href => "/projects/#{@project.id}/import"}
     end
+    
+    it "should not have an import all link" do
+      response.should_not have_selector :a, attribute = {:href => "/projects/#{@project.id}/import_all"}
+    end
   end
   
   context "Importing all outstanding features" do
@@ -56,6 +60,24 @@ describe "projects/show.html.erb" do
     
     it "should display an Import all link" do
       response.should have_selector :a, attribute = {:href => import_all_project_path(@project)}
+    end
+  end
+  
+  context "when there are no features to import" do
+    before(:each) do
+      @project.stub(:find).and_return @project
+      assigns[:project] = @project
+      @project.stub!(:location).and_return "#{RAILS_ROOT}"
+      @project.stub!(:features_to_import?).and_return false
+      render
+    end
+
+    it "does not display the import link" do
+      response.should_not have_selector :a, attribute = {:id => 'import'}
+    end
+    
+    it "does not have an import all link" do
+      response.should_not have_selector :a, attribute = {:href => "/projects/#{@project.id}/import_all"}
     end
   end
 end
