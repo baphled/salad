499983c7b896ef3230af0ecb5f30c428b65445f2
Yomi Colledge <baphled@boodah.net>
2010-07-13 10:01:09 +0100

    Renamed and refactored to reflect the change in the action name.

--- spec/views/features/source.html.erb_spec.rb
+++ spec/views/features/source.html.erb_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "/features/file.html.erb" do
+describe "/features/source.html.erb" do
   before(:each) do
     @file = File.read("#{RAILS_ROOT}/features/plain/tag_cloud.feature")
     assigns[:file] = @file
