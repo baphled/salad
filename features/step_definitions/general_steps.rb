ff86ff4124adad7be9532e51241e34e8088a3150
Yomi Colledge <yomi@boodah.net>
2010-07-14 22:51:13 +0100

    Refactored to make more specific

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,5 +1,5 @@
 Then /^a flash message '(.*)' should be displayed$/ do |message|
-  flash.should contain "#{message}"
+  flash[:notice].should contain "#{message}"
 end
 
 Then /^I should not be redirected to the form$/ do
