ab81ca827ddce94696c12eccfdbc834ede083920
Yomi Colledge <yomi@boodah.net>
2010-07-16 19:34:26 +0100

    Moved form based step into the form steps file

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -2,10 +2,6 @@ Then /^a flash message '(.*)' should be displayed$/ do |message|
   flash[:notice].should contain "#{message}"
 end
 
-Then /^I should not be redirected to the form$/ do
-  response.should_not have_selector :form
-end
-
 When /^we click the "([^\"]*)" feature$/ do |link|
   click_link "#{link}"
 end
