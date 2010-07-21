20b19f2c9b12510972c1f0fd936fdb7ab1f8bde3
Yomi Colledge <baphled@boodah.net>
2010-07-21 12:06:21 +0100

    Initial blueprint file

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -3,6 +3,9 @@ require 'sham'
 require 'faker'
 
 Sham.title { Faker::Lorem.words(3).join(' ') }
+Sham.feature { Faker::Lorem.sentence }
+Sham.scenario { Faker::Lorem.sentence }
+Sham.step { "Given #{Faker::Lorem.words(3).join(' ')}" }
 
 Project.blueprint {
   title { Sham.title }
@@ -11,8 +14,29 @@ Project.blueprint {
   location { "#{RAILS_ROOT}" }
 }
 
+Feature.blueprint {
+  title { Sham.title }
+  as_a { "user" }
+  in_order { Faker::Lorem.sentence }
+  i_want { Faker::Lorem.sentence }
+}
+
+Story.blueprint {
+  scenario { Sham.title }
+}
+
+Step.blueprint {
+  title { Sham.step }
+}
+
 def machine_fixtures
   3.times {
     Project.make
   }
+end
+
+def project_with_features(attributes = {})
+  Project.make(attributes) do |project|
+    3.times { project.features.make }    
+  end
 end
