4802400953389499a116dd3d396f84c589d1ff17
Yomi Colledge <yomi@boodah.net>
2010-08-05 23:52:35 +0100

    Added egrep to our import includes and removed the egrep function declaration from application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -111,28 +111,3 @@ $(document).ready(function() {
   
   $('table.ui-tabs tbody tr').hover(displayExamples, restoreExamples);
 });
-
-(function($) {
-  $.fn.egrep = function(pat) {
-   var out = [];
-   var textNodes = function(n) {
-    if (n.nodeType == Node.TEXT_NODE) {
-     var t = typeof pat == 'string' ?
-      n.nodeValue.indexOf(pat) != -1 :
-      pat.test(n.nodeValue);
-     if (t) {
-      out.push(n.parentNode);
-     }
-    }
-    else {
-     $.each(n.childNodes, function(a, b) {
-      textNodes(b);
-     });
-    }
-   };
-   return this.each(function() {
-    textNodes(this);
-   });
-   
-  };
-})(jQuery);
