c0780a95e9c89d0845b2283610055bbb23f6fe6f
Yomi Colledge <yomi@boodah.net>
2010-08-05 12:25:52 +0100

    More house keeping to make specs more readable.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -6,21 +6,21 @@ describe ApplicationController do
     response.should use_layout("no_sidebar")
   end
 
-  context "Projects" do
+  context "ProjectsController" do
     controller_name :projects
     before(:each) do
       Project.stub(:find).and_return mock_model(Project).as_null_object
     end
     
     describe "GET, new"  do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :new
         response.should use_layout("no_sidebar")
       end
     end
 
     describe "POST, create" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         @project = mock_model(Project).as_new_record.as_null_object
         @project.stub!(:save).and_return false
         Project.stub!(:new).and_return @project
@@ -29,19 +29,19 @@ describe ApplicationController do
     end
   
     describe "GET, edit" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :edit, {:id => 1}
       end
     end
     
     describe "GET, import" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :import, {:id => 1}
       end
     end
   end
   
-  context "Features" do
+  context "FeaturesController" do
     controller_name :features
 
     before(:each) do
@@ -50,38 +50,38 @@ describe ApplicationController do
     end
 
     describe "GET, export" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :export, {:id => 1}
       end
     end
     
     describe "GET, changes" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :changes, {:id => 1}
       end      
     end
 
     describe "GET, merge" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :merge, {:id => 1}
       end      
     end
     
     describe "GET, system_merge" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :system_merge, {:id => 1}
       end      
     end
     
     describe "GET, source" do
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         @feature.stub!(:path).and_return "#{RAILS_ROOT}/spec/fixtures/features/tag_cloud.feature"
         get :source, {:id => 1}
       end
     end
   end
   
-  context "Parking" do
+  context "ParkingController" do
     controller_name :parking
 
     before(:each) do
@@ -94,7 +94,7 @@ describe ApplicationController do
         Resource.stub!(:find).and_return @resource
       end
 
-      it "should render the no sidebar layout" do
+      it "renders the no sidebar layout" do
         get :tickets, {:resource => {:id => '1'}, :parking => {:tag => 'feature'}}
       end
     end
